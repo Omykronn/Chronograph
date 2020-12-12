788135f9a23360e9dd44c77a5487efb36b6d1f0e
Omyrkonn <omykronn.player@gmail.com>
2020-12-12 11:47:59 +0100

    Improvement of the Chart
    - Legend's Display with the Vectors' Colors
    - Automatic Chart Stretching to fit in all Navigator
    - Example available on GitHub
    
    - Deletion of the .github/workflows directory

--- Chronophotograph.py
+++ Chronophotograph.py
@@ -1,18 +1,17 @@
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import Arrow, VeeHead
+from bokeh.models import Arrow, VeeHead, Legend, LegendItem
 
-
-nb = 5  # Number of Point
+nb = 10  # Number of Point
 var = "t"  # Name of the used Variable
 
-equX = "2 * t**2 + t"  # Equation of the Movement on the X-Axis
-equY = "t**3 + 2*t**2 + 3*t + 4"  # Equation of the Movement on the Y-Axis
+equX = "2*t"  # Equation of the Movement on the X-Axis
+equY = "-2*t**2 + 15*t"  # Equation of the Movement on the Y-Axis
 
-speed_x = "4**t + 1"  # Equation of the X-Speed Value
-speed_y = "3*t**2 + 4*t + 3"  # Equation of the Y-Speed Value
+speed_x = "2"  # Equation of the X-Speed Value
+speed_y = "-4*t + 15"  # Equation of the Y-Speed Value
 
-acc_x = "5"  # Equation of the X-Acceleration Value
-acc_y = "6*t + 4"  # Equation of the Y-Acceleration Value
+acc_x = "0"  # Equation of the X-Acceleration Value
+acc_y = "-4"  # Equation of the Y-Acceleration Value
 
 # ------ END OF THE EDITABLE PARAMETER ------
 
@@ -33,20 +32,24 @@ acc_vector = eval("[({x}, {y}) for {var} in range({max})]".format(x=acc_x, y=acc
 output_file("Mechanic_Movement.html", title="Chromatograph")
 
 # Creation of the Canvas
-f = figure(plot_width=1900, plot_height=950, x_range=[-1, pos_x[-1] + speed_vector[-1][0] + 2],
-           y_range=[-1, pos_y[-1] + speed_vector[-1][1] + 2], tools="box_zoom,wheel_zoom,reset")
+f = figure(sizing_mode="stretch_both", x_axis_label="Abscissa", y_axis_label="Ordinate",
+           tools="box_zoom,wheel_zoom,reset")
 
-f.circle(pos_x, pos_y, size=10, color=pos_color)  # Draw each Point of the Movement
-
+f.circle(pos_x, pos_y, size=10, color=pos_color, name="pos")  # Draw each Point of the Movement
 
 # Drawing of each Speed and Acceleration Vectors
 for i in range(nb):
     f.add_layout(Arrow(end=VeeHead(size=10, line_color=speed_color, fill_color=speed_color), line_color=speed_color,
                        line_width=2, x_start=pos_x[i], y_start=pos_y[i], x_end=pos_x[i] + speed_vector[i][0],
-                       y_end=pos_y[i] + speed_vector[i][1]))  # Draw the Speed Vector at Point i+1
+                       y_end=pos_y[i] + speed_vector[i][1])) # Draw the Speed Vector at Point i+1
 
     f.add_layout(Arrow(end=VeeHead(size=10, line_color=acc_color, fill_color=acc_color), line_color=acc_color,
                        line_width=2, x_start=pos_x[i], y_start=pos_y[i], x_end=pos_x[i] + acc_vector[i][0],
                        y_end=pos_y[i] + acc_vector[i][1]))  # Draw the Acceleration Vector at Point i+1
+
+# Creating the Legend
+f.add_layout(Legend(items=[LegendItem(label="Position", renderers=f.select(name="pos")),
+                           LegendItem(label="Speed Vector (m/s)", renderers=[f.line(color=speed_color, line_width=2)]),
+                           LegendItem(label="Acceleration Vector (m/s²)", renderers=[f.line(color=acc_color, line_width=2)])]))
 
 show(f)
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Chronophotograph
 Python Program for Physical Movement's Study (Education)
 
-* With Graphical Output (with the Bokeh Module*)
+* With Graphical Output readable on all Internet Navigators (with the Bokeh Module*)
+* Automatic Chart Stretching
 * Show the Speed, and the Acceleration Vectors for each Point of the Movement
 
 *This Module may have to be downloaded
